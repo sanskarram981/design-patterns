621a44b97a455e7076d017ca4fcf7bfd382c4425
Sanskar Ram <sanskarram992@gmail.com>
2024-02-18 18:20:35 +0530

    singleton done

--- singleton design pattern/Singleton.java
+++ singleton design pattern/Singleton.java
@@ -1,12 +1,16 @@
-public class Singleton
+import java.io.Serial;
+import java.io.Serializable;
+
+public class Singleton implements Serializable,Cloneable
 {
     private static Singleton instance;
 
-    private Singleton(){
-
+    private Singleton() throws Exception{
+        if(instance != null)
+            throw new RuntimeException("Singleton is breaking!!");
     } 
 
-    public static Singleton getInstance(){
+    public static Singleton getInstance() throws Exception{
             synchronized(Singleton.class){
             if(instance == null){
             instance = new Singleton();
@@ -15,4 +19,14 @@ public class Singleton
          }
         return instance;
     }
+    @Serial
+    public Object readResolve(){
+        return instance;
+    }
+
+    public Object clone() throws CloneNotSupportedException {
+        //return super.clone();
+        return instance;
+    }
+
 }
